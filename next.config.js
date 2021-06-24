454fe8a04560ebfc788a9222e2c836ce74f83df0
Clayton Campos <clayton.almeida.campos@gmail.com>
2021-06-24 08:12:13 -0300

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  // other next config
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
